de45fa1329ff126f9f41b1dcb34cd5f8cf02c604
Lise Chen <liseche@uio.no>
2024-12-16 01:51:18 +0100

    reduce size

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -6,14 +6,10 @@ const Layout = () => {
   return (
     <>
     <div data-bs-theme="dark">
-      <nav class="navbar navbar-expand-lg bg-transparent">
-        <div class="container-fluid">
+      <nav class="navbar navbar-expand-lg bg-transparent fixed-top">
+        <div class="container">
           <a class="navbar-brand" href="#">Lise Chen</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#"><Link to="/">Home</Link></a>
               </li>
@@ -24,7 +20,6 @@ const Layout = () => {
                 <a class="nav-link" href="#"><Link to="/contact">Contact ☏</Link></a>
               </li>
             </ul>
-          </div>
         </div>
       </nav>
       <div class="container-fluid vh-100 d-flex justify-content-center align-items-center bg-transparent">
